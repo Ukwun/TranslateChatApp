c958d164df207365951d4102662e9e52d01ebcd4
John Solace <solaceinterlude@gmail.com>
2025-09-04 05:29:41 +0000

    Add global error boundary to catch and display errors instead of blank page

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,12 +4,15 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+import ErrorBoundary from './components/ErrorBoundary.jsx';
 import './i18n';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 );
